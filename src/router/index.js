f2224dc46cd0c8580b7552ffc9cb43c31dc497d8
devjmv <javier161890@gmail.com>
2024-06-11 22:12:47 +0200

    deleted comentri

--- src/router/index.js
+++ src/router/index.js
@@ -12,17 +12,11 @@ const router = createRouter({
     {
       path: '/planets',
       name: 'planets',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Planets.vue')
     },
     {
       path: '/favorite',
       name: 'favorite',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/Favorite.vue')
     }
   ]
