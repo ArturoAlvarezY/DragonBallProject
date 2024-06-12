f59ba40a31e557110cd9ecbaa66d5f695de2e373
devjmv <javier161890@gmail.com>
2024-06-12 11:37:42 +0200

    card updated

---
+++ src/components/card/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    ki: {
+        type: String,
+        required: true
+    },
+    raze: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="card">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Name: {{ name }}</h5>
+            <h5 class="card-title">Max Ki: {{ ki }}</h5>
+            <h5 class="card-title">Raze: {{ raze }}</h5>
+            <h5 class="card-title">Description:</h5>
+            <p class="card-text">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Card from '../components/card/Card.vue'
 </script>
 
 <template>
   <main>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col">
+        <Card name="Goku" ki="10000" raze="Sajayin" description="Description"
+          image="https://imgs.search.brave.com/fWNyQ2cf5_ixlSYnGSOaqjQchtbyNWm584EdTBac3mc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nbWFydC5jb20v/ZmlsZXMvMi9Hb2t1/LVBORy1IRC5wbmc" />
+      </div>
+    </div>
     <TheWelcome />
   </main>
 </template>
