b0e8463f1bbd1dbfa6c9c54f7594f877e491f7ed
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-17 19:56:38 +0200

    created NavBar.vue component only need to add the logo and modify two things. Added in CORE: in MODELS: Repository.js, Planets.js, Character.js and in APIS - DRAGONBALL: DragonBallService.js, DragonBallRepository.js. Added almost all the code to have our API without coupling to anything

---
+++ src/components/core/apis/dragonBall/DragonBallService.js
@@ -0,0 +1,30 @@
+import Character from "../../models/Character.js"
+import Repository from "../../models/Repository.js"
+
+export default class DragonBallService {
+    #repo
+
+    constructor(repository) {
+        this.#repo = Repository
+    }
+
+    async getCharacter() {
+        const data = await this.#repo.getById('Character')
+
+        const character = data.DragonBall.item.map( (item) => {
+            return new Character(item.name, item.img, item.id, item.ki, item.race, item.description)
+        })
+
+        return character
+    }
+
+    async getPlanets() {
+        const data = await this.#repo.getById('Planets')
+
+        const planets = data.DragonBall.item.map( (item) => {
+            return new Planet(item.name, item.img, item.id, item.isDestroyed)
+        })
+
+        return planets
+    }
+}
