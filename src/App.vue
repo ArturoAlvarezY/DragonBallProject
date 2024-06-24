ca0f3d9576d4d0b56c4949cfaf8d20cfa279f72a
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-06-24 14:10:58 +0200

    null

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './views/NavBar.vue';
 </script>
 
 <template>
+  <nav>
+    <NavBar />
+  </nav>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/planets">Planets</RouterLink>
         <RouterLink to="/favorite">Favorite</RouterLink>
         
       </nav>
-    </div>
   </header>
 
   <RouterView />
